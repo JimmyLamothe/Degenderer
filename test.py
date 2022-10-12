b983e5b566ea58cd9eb7924ca4abbd4ad2b945b4
JimmyLamothe <jmichel.dev@gmail.com>
2022-10-12 10:53:42 -0400

    Ability to modify epubs with ebooklib confirmed

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from degenderer import add_foo, check_out, create_epub, create_soup, get_book_items
-from degenderer import get_paragraph_list, item_to_soup, soup_to_book
+import sys
+
+from librarian import add_foo, check_out, create_epub, create_soup, get_book_items
+from librarian import get_paragraph_list, item_to_soup, soup_to_book
 
 book = check_out('Jane Eyre')
 
@@ -7,10 +9,15 @@ book_soup = create_soup(book)
 
 paragraph_list = get_paragraph_list(book_soup)
 
+if not paragraph_list:
+    print('Failed to get paragraph list')
+    sys.exit(0)
+
 for paragraph in paragraph_list:
     try:
         add_foo(paragraph)
     except AttributeError:
+        input('Attribute Error. Press enter to continue')
         pass
 
 soup_to_book(book_soup, book)
