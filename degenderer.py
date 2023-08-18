c83362ff8763269f990dd379e80d59d8b130d66b
JimmyLamothe <jmichel.dev@gmail.com>
2023-08-18 14:01:59 -0400

    Cleanup - removed outdated functions

--- degenderer.py
+++ degenderer.py
@@ -112,27 +112,6 @@ def get_name_dict(book_soup, verbose=False):
 def get_sorted_name_list(name_dict, verbose=False):
     return lazy_shuffle_keys(name_dict, reverse=True)
     
-def get_period_names(year, verbose=False):
-    """ NOT CURRENTLY USED """
-    if gender == 'm':
-        print('m')
-        name_list = BOY_NAMES_BY_DECADE
-    elif gender == 'f':
-        print('f')
-        name_list = GIRL_NAMES_BY_DECADE
-    else:
-        if not gender == 'nb':
-            print('WARNING: Unknown gender in parameters, defaulting to non-binary')
-        else:
-            print('nb')
-            name_list = NB_NAMES_BY_DECADE
-    target_decade = year + 30
-    name_diff_dict = {item : get_min_diff(year, value)
-                         for (item, value)in name_list.items()}
-    period_names = lazy_shuffle_keys(name_diff_dict, reverse=False)
-    period_names.extend(NB_NAMES_MODERN)
-    return period_names
-
 def change_name(name, match, text, verbose=False):
     if False:#verbose:
         print('Matching for : ' + name)
@@ -160,45 +139,9 @@ def degender_text(text, parameters):
             print(text)
     return text
 
-def degender_string(string, parameters):
-    if parameters['verbose']:
-        print('Pre-text:')
-        print(string)
-    new_text = degender_text(string, parameters)
-    if parameters['verbose']:
-        print('Post-text:')
-        print(new_text)
-    if new_text != string:
-        print('returning new text')
-        return new_text
-    else:
-        return False
-
-def degender_tag(tag, tag_type, parameters):
-    print(f'pre {tag_type}')
-    print(tag)
-    new_contents = []
-    for (number, child) in enumerate(tag.contents):
-        print(number)
-        print(child)
-        if is_string(child):
-            new_string = degender_string(child, parameters)
-            if new_string:
-                child.replace_with(new_string)
-        print(f'post {tag_type}')
-        print(tag)
-        #pause()
-    
-def degender_soup(soup, parameters):
-    for div in get_divs(soup):
-        degender_tag(div, 'div', parameters)
-    for p in get_paragraphs(soup):
-        degender_tag(p, 'paragraph', parameters)
-
 def degender_all(item, parameters):
-    """ Test function to replace degender_soup """
     if is_string(item):
-        new_string = degender_string(item, parameters)
+        new_string = degender_text(item, parameters)
         if new_string:
             item.replace_with(new_string)
     try:
@@ -207,36 +150,11 @@ def degender_all(item, parameters):
                 degender_all(child, parameters)    
     except AttributeError:
         pass
-    
-def get_all_name_matches(name_list, parameters):
-    """ NOT CURRENTLY USED """
-    period_names = get_period_names(parameters['year'])
-    return list(zip(name_list, period_names))
-    
-def get_name_matches(name_list, parameters):
-    """ NOT CURRENTLY USED """
-    name_matches = {}
-    name_match_list = get_all_name_matches(name_list, parameters)
-    import sys
-    print('Enter a name for each main character or press ENTER to accept suggestion')
-    for name, suggestion in name_match_list[0:parameters['name choices']]:
-        #NOTE: functionality removed for web compatibility
-        new_name = '' #input(f'Name (suggestion: {suggestion}): ') 
-        if not new_name:
-            name_matches[name] = suggestion
-        else:
-            name_matches[name] = new_name
-    return name_matches
-    
+        
 def degender_book(book_soup, parameters = DEFAULT_PARAMETERS):
     parameters = fill_defaults(parameters)
     print(parameters)
-    #name_dict = get_name_dict(book_soup)
-    #name_list = get_sorted_name_list(drop_low(name_dict)) #Removing names with few occurrences
-    #parameters['name matches'] = get_name_matches(name_list, parameters)
-    #REMOVED PREVIOUS 2 LINES FOR WEB VERSION
     for soup in book_soup:
-        #degender_soup(soup, parameters)
         degender_all(soup, parameters)
 
 def get_text_dict(text):
